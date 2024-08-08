551758477c8b5e37c5621a716bf4d74fbec05b06
jooyong-boo <qnwndyd159@naver.com>
2024-08-09 00:59:32 +0900

    chore: svgr 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    output: "standalone",
+  reactStrictMode: true,
+  output: "standalone",
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
