83c539a7d4c50bcc8387ff079ff6a15a89353874
jooyong-boo <qnwndyd159@naver.com>
2024-08-14 21:47:04 +0900

    feature: 다크모드 기본 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`min-h-screen ${inter.className}`}>
+      <body className={`min-h-screen dark:bg-slate-900 ${inter.className}`}>
         <Header />
         {children}
       </body>
--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -5,28 +5,48 @@ import LogoWhite from "@/assets/img/logo_white.png";
 import DarkModeSvg from "@/assets/svg/dark_mode.svg";
 import LightModeSvg from "@/assets/svg/light_mode.svg";
 import useThemeStore from "@/store/useThemeStore";
-import Image from "next/image";
 import Link from "next/link";
 import { useStore } from "zustand";
+
+const HeaderMenu = [
+  { title: "Home", href: "/" },
+  { title: "Posts", href: "/posts" },
+  { title: "Tags", href: "/tags" },
+  { title: "About", href: "/about" },
+];
 
 function Header() {
   const { theme, toggleTheme } = useStore(useThemeStore);
 
   return (
     <div className="w-full flex items-center justify-between min-h-14 px-4 py-4">
-      <Link href="/">
-        <Image src={LogoWhite} alt="logo" width={100} height={100} />
+      <Link href="/" className="text-slate-900 dark:text-slate-50">
+        {/* <Image
+          src={theme === "dark" ? LogoWhite : LogoBlack}
+          alt="logo"
+          width={100}
+          height={100}
+        /> */}
+        개발블로그
       </Link>
       <div className="flex items-center gap-4">
         <div className="flex items-center gap-2">
-          <Link href="/posts">Posts</Link>
-          <Link href="/tags">Tags</Link>
-          <Link href="/about">About</Link>
+          {HeaderMenu.map((menu) => (
+            <Link
+              key={menu.title}
+              href={menu.href}
+              className="text-gray-800 dark:text-gray-200"
+            >
+              {menu.title}
+            </Link>
+          ))}
         </div>
         <div className="flex items-center gap-2">
           <button onClick={toggleTheme}>
-            {theme === "dark" && <DarkModeSvg className="w-6 h-6" />}
-            {!theme && <LightModeSvg className="w-6 h-6" />}
+            {theme === "dark" && (
+              <DarkModeSvg className="w-6 h-6 fill-slate-50" />
+            )}
+            {!theme && <LightModeSvg className="w-6 h-6 fill-slate-900" />}
           </button>
         </div>
       </div>
