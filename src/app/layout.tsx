3f7e05353e78181fbcde74b9242d563014828de8
jooyong-boo <qnwndyd159@naver.com>
2024-08-24 12:37:33 +0900

    feature: react-hot-toast 초기 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,40 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { DefaultToastOptions, Toaster } from 'react-hot-toast';
 import Footer from '@/layouts/Footer';
 import Header from '@/layouts/Header/index';
+import useThemeStore from '@/store/useThemeStore';
+import { tailwindTheme } from '@/utils/tailwindTheme';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'DEVLOG - jooyong',
   description: "jooyong's devlog",
+};
+
+export const toasterOptions: DefaultToastOptions = {
+  style: {
+    minWidth: '100px',
+    maxWidth: '380px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: '10px',
+    borderRadius: '40px',
+    background:
+      useThemeStore.getState().theme === 'dark'
+        ? tailwindTheme.colors.slate[50]
+        : tailwindTheme.colors.slate[900],
+    color:
+      useThemeStore.getState().theme === 'dark'
+        ? tailwindTheme.colors.slate[900]
+        : tailwindTheme.colors.slate[50],
+    fontSize: '1rem',
+    // TODO: theme dark 적용 안됨 해결하기
+    // ...theme.typography.buttonM,
+  },
 };
 
 export default function RootLayout({
@@ -19,10 +45,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`min-h-screen flex flex-col bg-slate-50 dark:bg-slate-900 text-slate-900 dark:text-slate-50 ${inter.className}`}
+        className={`flex min-h-screen flex-col bg-slate-50 text-slate-900 dark:bg-slate-900 dark:text-slate-50 ${inter.className}`}
       >
+        <Toaster position="top-center" toastOptions={toasterOptions} />
         <Header />
-        <div className="flex-grow flex">{children}</div>
+        <div className="flex flex-grow">{children}</div>
         <Footer />
       </body>
     </html>
