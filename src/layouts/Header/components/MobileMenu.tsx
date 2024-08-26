19247fe3f871a253788a0764fa9f87c7e85f291c
jooyong-boo <qnwndyd159@naver.com>
2024-08-26 18:39:46 +0900

    feature: 로그인 메뉴 추가

--- src/layouts/Header/components/MobileMenu.tsx
+++ src/layouts/Header/components/MobileMenu.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Link from 'next/link';
-import MenuSvg from '@/assets/svg/menu.svg';
+import { Menu, Github, Google } from '@/assets/svg/index';
 import { menus } from '@/layouts/Header/constants/menu';
 import useActive from '@/layouts/Header/hooks/useActive';
 
@@ -20,10 +20,20 @@ function MobileMenu() {
   return (
     <div className="flex sm:hidden">
       <button onClick={handleMenuOpen}>
-        <MenuSvg className="h-6 w-6 fill-slate-900 dark:fill-slate-50" />
+        <Menu className="h-6 w-6 fill-slate-900 dark:fill-slate-50" />
       </button>
       {isMenuOpen && (
         <div className="absolute left-0 top-14 w-full bg-slate-50 shadow-lg dark:bg-slate-900">
+          <div className="mb-4 flex justify-center gap-4 border-b pb-4">
+            <div className="flex flex-col items-center gap-0.5 text-sm">
+              <Github width={40} height={40} />
+              <p>Github</p>
+            </div>
+            <div className="flex flex-col items-center gap-0.5 text-sm">
+              <Google width={40} height={40} />
+              <p>Google</p>
+            </div>
+          </div>
           <div className="flex flex-col items-center gap-4 py-4">
             {menus.map((menu) => (
               <Link
