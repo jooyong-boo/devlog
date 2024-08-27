672ae30212ba328a42ed0c5769c0497c5385c0a0
jooyong-boo <qnwndyd159@naver.com>
2024-08-27 22:04:32 +0900

    refactor: 중복된 main 태그 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,17 @@ import ArticleList from '@/containers/post/ArticleList';
 import InnerLayout from '@/layouts/InnerLayout';
 export default function Home() {
   return (
-    <main className="init-layout">
-      <InnerLayout>
-        <Title size="large" borderBottom>
-          최신글
-        </Title>
-        <ArticleList />
-        <div className="flex justify-end">
-          <CustomLink href="/posts">
-            모든 글 보기
-            <ArrowRight width={16} height={16} />
-          </CustomLink>
-        </div>
-      </InnerLayout>
-    </main>
+    <InnerLayout>
+      <Title size="large" borderBottom>
+        최신글
+      </Title>
+      <ArticleList />
+      <div className="flex justify-end">
+        <CustomLink href="/posts">
+          모든 글 보기
+          <ArrowRight width={16} height={16} />
+        </CustomLink>
+      </div>
+    </InnerLayout>
   );
 }
