76daae3a8f21f71a84492c86d1497f76a6f36668
jooyong-boo <qnwndyd159@naver.com>
2024-08-29 01:17:15 +0900

    feature: 로고 이미지 추가

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,18 +1,14 @@
-'use client';
-
-import Link from 'next/link';
-import Darkmode from '@/layouts/Header/components/Darkmode';
+import DarkmodeProvider from '@/layouts/Header/components/DarkmodeProvider';
+import Logo from '@/layouts/Header/components/Logo';
 import MobileMenu from '@/layouts/Header/components/MobileMenu';
 import PCMenu from '@/layouts/Header/components/PCMenu';
 
 function Header() {
   return (
-    <header className="sticky top-0 z-50 flex min-h-14 w-full items-center justify-between bg-slate-100 px-4 py-4 dark:bg-slate-800">
-      <Link href="/" className="text-slate-900 dark:text-slate-50">
-        개발블로그
-      </Link>
+    <header className="sticky top-0 z-50 flex h-16 w-full items-center justify-between bg-slate-100 px-4 py-4 dark:bg-slate-800">
+      <Logo />
       <div className="flex items-center gap-4">
-        <Darkmode />
+        <DarkmodeProvider />
         <PCMenu />
         <MobileMenu />
       </div>
