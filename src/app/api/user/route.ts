ac3c846c718de6f539262fcf358a7e1e6613c7fc
jooyong-boo <qnwndyd159@naver.com>
2024-09-02 17:48:35 +0900

    refactor: prisma가 대부분의 경우 알아서 처리해준다고 해서 명시적인 prisma 연결 종료하는 코드 제거

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,10 +1,57 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
 import { oAuthProviders, SignUpRequestData } from '@/types/auth';
 import prisma from '../../../../prisma/client';
 
 export async function GET(req: NextRequest) {
-  const users = await prisma.users.findMany();
-  return NextResponse.json({ users }, { status: 200 });
+  try {
+    // 쿠키 확인
+    const token = await getToken({
+      req,
+      secret: process.env.AUTH_SECRET!,
+      secureCookie: process.env.NODE_ENV === 'production',
+      salt:
+        process.env.NODE_ENV === 'production'
+          ? '__Secure-authjs.session-token'
+          : 'authjs.session-token',
+    });
+
+    if (token === null || !token.email) {
+      return NextResponse.json(
+        { message: '로그인이 필요합니다.' },
+        { status: 401 },
+      );
+    }
+
+    const user = await prisma.users.findUnique({
+      where: {
+        email: token.email,
+      },
+      select: {
+        id: true,
+        email: true,
+        nickname: true,
+        profile: true,
+        oauthProvider: {
+          select: {
+            name: true,
+          },
+        },
+        role: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json({ user }, { status: 200 });
+  } catch (e) {
+    return NextResponse.json(
+      { message: '회원 정보 조회 중 오류가 발생했습니다.' },
+      { status: 500 },
+    );
+  }
 }
 
 export async function POST(req: NextRequest) {
@@ -52,7 +99,5 @@ export async function POST(req: NextRequest) {
       { message: '회원가입 처리 중 오류가 발생했습니다.' },
       { status: 500 },
     );
-  } finally {
-    await prisma.$disconnect();
   }
 }
