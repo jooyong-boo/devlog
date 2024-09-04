f32c38e53705f81b441395bfee8a9af745f1ebba
jooyong-boo <qnwndyd159@naver.com>
2024-09-04 19:05:46 +0900

    edit: 에러로직 간소화 및 리턴 타입이 꼬여있던 문제 수정

--- src/services/customAxios.ts
+++ src/services/customAxios.ts
@@ -1,18 +1,11 @@
-import axios, { AxiosError, AxiosRequestConfig } from 'axios';
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 import server from '@/services/index';
-
-const handleError = <T>(error: unknown): Promise<T> => {
-  if (axios.isAxiosError(error) && typeof error.message === 'string') {
-    throw new Error(error.message);
-  }
-  throw new Error((error as AxiosError<T>).message);
-};
 
 export const getData = async <T>(
   url: string,
   config?: AxiosRequestConfig,
   queryString?: { [key: string]: string | number | boolean | undefined },
-): Promise<T> => {
+) => {
   try {
     const searchParams = new URLSearchParams();
     if (queryString) {
@@ -26,10 +19,10 @@ export const getData = async <T>(
       searchParams.toString() ? `?${searchParams.toString()}` : ''
     }`;
 
-    const response = await server.get<T>(fullUrl, config);
+    const response: AxiosResponse<T> = await server.get<T>(fullUrl, config);
     return response.data;
   } catch (error) {
-    return handleError(error);
+    throw error;
   }
 };
 
@@ -37,12 +30,12 @@ export const postData = async <T>(
   url: string,
   data?: unknown,
   config?: AxiosRequestConfig,
-): Promise<T> => {
+) => {
   try {
     const response = await server.post<T>(url, data, config);
     return response.data;
   } catch (error) {
-    return handleError(error);
+    throw error;
   }
 };
 
@@ -52,10 +45,10 @@ export const putData = async <T>(
   config?: AxiosRequestConfig,
 ): Promise<T> => {
   try {
-    const response = await server.put<T>(url, data, config);
+    const response: AxiosResponse<T> = await server.put<T>(url, data, config);
     return response.data;
   } catch (error) {
-    return handleError(error);
+    throw error;
   }
 };
 
@@ -63,23 +56,23 @@ export const patchData = async <T>(
   url: string,
   data?: unknown,
   config?: AxiosRequestConfig,
-): Promise<T> => {
+) => {
   try {
-    const response = await server.patch<T>(url, data, config);
+    const response: AxiosResponse<T> = await server.patch<T>(url, data, config);
     return response.data;
   } catch (error) {
-    return handleError(error);
+    throw error;
   }
 };
 
 export const deleteData = async <T>(
   url: string,
   config?: AxiosRequestConfig,
-): Promise<T> => {
+) => {
   try {
-    const response = await server.delete<T>(url, config);
+    const response: AxiosResponse<T> = await server.delete<T>(url, config);
     return response.data;
   } catch (error) {
-    return handleError(error);
+    throw error;
   }
 };
