80d5d6e7bf159a1878965dc309a9bce41bbb3329
jooyong-boo <qnwndyd159@naver.com>
2024-09-07 22:11:04 +0900

    edit: fetch 요청 대신 prisma를 통해 db에 직접 접근하도록 수정 테스트

--- src/services/posts.ts
+++ src/services/posts.ts
@@ -1,20 +1,33 @@
 import { getData, postData } from '@/services/customAxios';
 import { PostDetailNavigationResponse } from '@/types/post';
-import { FormattedPost } from '@/types/post.prisma';
+import { postQueryOptions } from '@/types/post.prisma';
 import { FormattedPostDetail } from '@/types/postDetail.prisma';
+import prisma from '../../prisma/client';
 
 interface GetPostsRequest {
   cursor?: string;
 }
 
-export const getPosts = async ({
-  cursor,
-}: GetPostsRequest): Promise<FormattedPost[]> =>
-  getData('/api/posts', {
-    params: {
-      cursor,
-    },
-  });
+export const getPosts = async ({ cursor }: GetPostsRequest) => {
+  try {
+    const postLists = await prisma.posts.findMany({
+      ...postQueryOptions,
+      skip: cursor ? 1 : 0,
+      ...(cursor && { cursor: { id: cursor } }),
+    });
+
+    const formattedTags = postLists.map((post) => {
+      return {
+        ...post,
+        postTag: post.postTag.map((tag) => tag.tags),
+      };
+    });
+
+    return formattedTags;
+  } catch (e) {
+    throw e;
+  }
+};
 
 export const getPostDetail = async (id: string): Promise<FormattedPostDetail> =>
   getData<FormattedPostDetail>(`/api/posts/${id}`);
