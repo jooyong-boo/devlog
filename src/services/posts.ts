8a36d91940972ec193a34eb55a7b60802ec05856
jooyong-boo <qnwndyd159@naver.com>
2024-09-13 00:45:54 +0900

    feature: 글 생성 axios 추가

--- src/services/posts.ts
+++ src/services/posts.ts
@@ -1,6 +1,6 @@
 import { getData, postData } from '@/services/customAxios';
-import { PostDetailNavigationResponse } from '@/types/post';
-import { postQueryOptions } from '@/types/post.prisma';
+import { CreatePostRequest, PostDetailNavigationResponse } from '@/types/post';
+import { FormattedPost, postQueryOptions } from '@/types/post.prisma';
 import { FormattedPostDetail } from '@/types/postDetail.prisma';
 import prisma from '../../prisma/client';
 
@@ -27,6 +27,26 @@ export const getPosts = async ({ cursor }: GetPostsRequest) => {
   } catch (e) {
     throw e;
   }
+};
+
+export const createPost = async ({
+  title,
+  content,
+  tags,
+  thumbnail,
+  published,
+  url,
+  projectId,
+}: CreatePostRequest): Promise<FormattedPost> => {
+  return await postData('/api/posts', {
+    title,
+    content,
+    tags,
+    thumbnail,
+    published,
+    url,
+    projectId,
+  });
 };
 
 export const getPostDetail = async (id: string): Promise<FormattedPostDetail> =>
