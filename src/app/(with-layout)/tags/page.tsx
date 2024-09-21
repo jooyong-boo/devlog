4366167150c9ba1d06a8a6efc20abdaea02cb92a
jooyong-boo <qnwndyd159@naver.com>
2024-09-21 21:58:01 +0900

    feature: 태그 조회 페이지 기능 추가

--- src/app/(with-layout)/tags/page.tsx
+++ src/app/(with-layout)/tags/page.tsx
@@ -1,5 +1,19 @@
-function page() {
-  return <div>작업중입니다.</div>;
-}
+import Tags from '@/components/Tags';
+import Title from '@/components/Title';
+import InnerLayout from '@/layouts/InnerLayout';
+import { getTags } from '@/services/tags';
+
+const page = async () => {
+  const tagList = await getTags();
+
+  return (
+    <InnerLayout className="gap-8">
+      <Title size="large" borderBottom>
+        태그
+      </Title>
+      <Tags tagList={tagList} />
+    </InnerLayout>
+  );
+};
 
 export default page;
