079c85c6f224bb677eea857d08420c4f768e2743
jooyong-boo <qnwndyd159@naver.com>
2024-09-23 14:47:44 +0900

    refactor: Tag 타입 세분화로 인한 수정

--- src/types/post.prisma.ts
+++ src/types/post.prisma.ts
@@ -1,5 +1,5 @@
 import { Prisma } from '@prisma/client';
-import { TagsResult } from '@/types/tags.prisma';
+import { TagWithCount } from '@/types/tags.prisma';
 
 export const postQueryOptions = {
   select: {
@@ -33,5 +33,5 @@ export type PostResult = Prisma.PostsGetPayload<{
 }>;
 
 export type FormattedPost = Omit<PostResult, 'postTag'> & {
-  postTag: TagsResult[];
+  postTag: TagWithCount[];
 };
