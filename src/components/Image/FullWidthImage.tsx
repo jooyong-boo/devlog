5522c7096582216e363d67f7fa5f98b38916bd05
jooyong-boo <qnwndyd159@naver.com>
2024-09-23 14:46:17 +0900

    edit: 이미지 비율 고정 및 sizes 세분화

--- src/components/Image/FullWidthImage.tsx
+++ src/components/Image/FullWidthImage.tsx
@@ -2,24 +2,19 @@ import Image from 'next/image';
 import { cn } from '@/utils/cn';
 
 interface FullWidthImageProps {
-  aspectRatio: string;
   image?: string | null;
   name: string;
 }
 
-const FullWidthImage = ({
-  image,
-  name,
-  aspectRatio = '4/2.5',
-}: FullWidthImageProps) => {
+const FullWidthImage = ({ image, name }: FullWidthImageProps) => {
   return (
-    <div className={cn(`relative aspect-[${aspectRatio}] sm:aspect-square`)}>
+    <div className={cn(`relative aspect-[4/2.5] w-full sm:aspect-square`)}>
       <Image
         src={image || 'https://placehold.co/600x400'}
         alt={name}
         fill
         priority
-        sizes="100vw"
+        sizes="(max-width: 640px) 100vw, (min-width: 640px) 50vw, 33vw"
         className="object-cover"
       />
     </div>
