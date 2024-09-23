cc78cdf1cbf55f3649f29ba8dae2307296d2c39d
jooyong-boo <qnwndyd159@naver.com>
2024-09-23 14:48:09 +0900

    edit: 이미지 비율 고정으로 인한 aspectRatio 속성 제거

--- src/containers/project/Item.tsx
+++ src/containers/project/Item.tsx
@@ -7,7 +7,7 @@ import { ProjectResult } from '@/types/project.prisma';
 const Item = ({ desc, id, image, name }: ProjectResult) => {
   return (
     <div className="flex w-full flex-col overflow-hidden rounded-md border border-slate-300 dark:border-slate-600">
-      <FullWidthImage image={image} name={name} aspectRatio="4/1.5" />
+      <FullWidthImage image={image} name={name} />
       <div className="flex flex-col gap-2 px-6 py-4">
         <h2 className="text-2xl">{name}</h2>
         {desc && <p className="text-overflow-3 text-slate-400">{desc}</p>}
