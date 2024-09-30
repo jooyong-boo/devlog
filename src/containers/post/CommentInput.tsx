eeffa32e72d5bd9567a8fca9e53b5f3eeb156baa
jooyong-boo <qnwndyd159@naver.com>
2024-09-30 15:34:10 +0900

    feature: 댓글 작성시 비로그인이면 toast 메시지

--- src/containers/post/CommentInput.tsx
+++ src/containers/post/CommentInput.tsx
@@ -2,14 +2,17 @@
 
 import { useState } from 'react';
 import { useParams, useRouter } from 'next/navigation';
+import { useSession } from 'next-auth/react';
 import Button from '@/components/Button';
 import Textarea from '@/components/Textarea';
 import useToast from '@/hooks/useToast';
 import { postComment } from '@/services/posts';
 
 const CommentInput = () => {
-  const router = useRouter();
   const params: { id?: string } = useParams();
+  const router = useRouter();
+  const { data: session } = useSession();
+
   const { enqueueWarningBar } = useToast();
 
   const [content, setContent] = useState('');
@@ -21,6 +24,11 @@ const CommentInput = () => {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    if (!session) {
+      enqueueWarningBar('로그인이 필요합니다.', 'error');
+      return;
+    }
 
     const { id } = params;
 
