861331af2050a61215eedf03fd4842dc4153ed2f
jooyong-boo <qnwndyd159@naver.com>
2024-10-11 14:16:37 +0900

    feature: 유저 정보 수정시 이미지 업로드 추가

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 import { auth } from '@/auth';
+import { postImages } from '@/services/images';
 import { oAuthProviders, SignUpRequestData } from '@/types/auth';
 import prisma from '../../../../prisma/client';
 
@@ -115,29 +116,51 @@ export async function PATCH(req: NextRequest) {
     }
 
     const { id } = session.user;
-    const { nickname, profile } = await req.json();
+    // formData로 전달된 데이터를 추출
+    const formData = await req.formData();
+    const nickname = formData.get('nickname') as string | undefined;
+    const profile = formData.get('profile') as File | undefined;
 
     // 필수 필드 확인
-    if (!id || !nickname || !profile) {
+    if (!id) {
       return NextResponse.json(
-        { message: '필수 필드가 누락되었습니다.' },
+        { message: '로그인이 필요합니다.' },
         { status: 400 },
       );
+    }
+
+    // 사용자의 이미지 s3에 업로드
+    const { imageUrl } = profile
+      ? await postImages({
+          file: profile,
+          folder: `users/${id}`,
+        })
+      : { imageUrl: '' };
+
+    console.log('imageUrl', imageUrl);
+
+    if (imageUrl) {
+      await prisma.users.update({
+        where: { id },
+        data: {
+          profile: imageUrl,
+        },
+      });
     }
 
     const updatedUser = await prisma.users.update({
       where: { id },
       data: {
         nickname,
-        profile,
       },
     });
 
     return NextResponse.json(
-      { message: '회원 정보가 수정되었습니다.', user: updatedUser },
+      { message: '회원 정보가 수정되었습니다.', userInfo: updatedUser },
       { status: 200 },
     );
   } catch (e) {
+    console.log(e);
     return NextResponse.json(
       { message: '회원 정보 수정 중 오류가 발생했습니다.' },
       { status: 500 },
