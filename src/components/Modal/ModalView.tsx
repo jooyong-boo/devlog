3934de73cf88cc0d06540320e1d44a2be23d99dd
jooyong-boo <qnwndyd159@naver.com>
2024-10-18 22:58:27 +0900

    edit: modal내 닫기 아이콘 위치 수정

--- src/components/Modal/ModalView.tsx
+++ src/components/Modal/ModalView.tsx
@@ -1,4 +1,5 @@
 import { ReactNode, useEffect } from 'react';
+import { Close } from '@/assets/svg/index';
 import { cn } from '@/utils/cn';
 
 interface ModalViewProps {
@@ -32,12 +33,12 @@ const ModalView = ({
   const overlayClasses = `fixed inset-0 z-[99] w-full h-full backdrop-blur  bg-black bg-opacity-50`;
 
   const modalClasses = cn(
-    `fixed left-1/2 transition-all duration-500 ease-in-out overflow-y-auto  sm:max-w-[500px] sm:mx-auto`,
+    `fixed left-1/2 transition-all duration-500 ease-in-out overflow-y-auto  sm:max-w-[700px] sm:mx-auto`,
     `dark:bg-gray-800 bg-gray-200 p-4 border dark:bg-slate-800 dark:border-slate-700`,
     'top-1/2 w-[calc(100%-32px)] -translate-x-1/2 -translate-y-1/2 rounded-lg animate-modalShow max-h-[90vh]',
   );
 
-  const closeBtnClasses = `absolute bg-transparent -top-8 right-0 text-gray-50 ${displayClose === 'none' ? 'hidden' : 'block'}`;
+  const closeBtnClasses = `absolute bg-transparent right-2 text-gray-50 ${displayClose === 'none' ? 'hidden' : 'block'}`;
 
   return (
     <div className={overlayClasses} onClick={onClose}>
@@ -46,13 +47,13 @@ const ModalView = ({
         className={modalClasses}
         onClick={(e) => e.stopPropagation()}
       >
-        {children}
         <button
-          className={`${closeBtnClasses} material-symbols-rounded`}
+          className={`${closeBtnClasses} fill-slate-700 dark:fill-slate-400`}
           onClick={onClose}
         >
-          close
+          <Close />
         </button>
+        {children}
       </div>
     </div>
   );
